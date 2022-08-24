1d3de300fa60973dd20058582b803c376f2b084c
Alvin <alvintob@gmail.com>
2022-08-24 09:56:44 -0400

    Initial Commit

---
+++ Isabel-Chat/Isabel_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Isabel_ChatApp.swift
+//  Isabel-Chat
+//
+//  Created by Jainy Alvin on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Isabel_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
